37dddefc94529ee9748337efd26f0e1784e49d5d
Yuvaraj <yuvaraj.n@askpundit.com>
2018-02-21 14:33:48 +0530

    Initial commit

---
+++ app/src/main/java/sd/com/myyoutubeplayer/utils/Config.java
@@ -0,0 +1,14 @@
+package sd.com.myyoutubeplayer.utils;
+
+/**
+ * Created by Yuvi on 08/02/18
+ */
+
+public class Config {
+    
+
+    public static final String DEVELOPER_KEY = "PASTE_YOUR_YOUTUBE_API_KEY_HERE";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "fY2FCWz8wOg";
+}
